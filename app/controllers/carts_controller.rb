bac72a5b74c6592f980979a890a4611e26120404
Kelsey Huse <kelseyhuse30@gmail.com>
2017-05-04 13:47:44 -0400

    Current user helper and Cart Controller methods

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,26 @@
 class CartsController < ApplicationController
+	before_action :set_cart!, only: [:show]
 
+	def show
+
+	end
+
+	def new
+		@cart = Cart.new
+	end
+
+	def create
+		@cart = Cart.create(cart_params)
+		current_user.current_cart = @cart
+	end
+
+	private
+
+	def set_cart!
+		Cart.find_by_id(params[:id])
+	end
+
+	def cart_params
+		params.require(:cart).permit(:user_id)
+	end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  # def current_cart
+  # 	session[:cart] ||= []
+  # end
+
+  def current_user
+  	@current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+ 	end
 end
