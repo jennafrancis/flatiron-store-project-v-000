7ecef0ac5b6257bd3f0c5cd9d1cb32cb4f97ddb1
jennafrancis <jef399@nyu.edu>
2017-05-05 11:08:12 -0400

    adds item to cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,14 +1,25 @@
 class LineItemsController < ApplicationController
 
-	def create
-		if !current_user.current_cart
-			@cart = Cart.create(status: "new", user_id: current_user.id)
-			current_user.current_cart = @cart
-			current_user.save
-		end
-		current_user.current_cart.add_item(item_id: params[:item_id])
-		current_user.current_cart.save
-		redirect_to cart_path(current_user.current_cart)
-	end
+	# def create
+	# 	binding.pry
+	# 	if !current_user.current_cart
+	# 		@cart = Cart.create(status: "new", user_id: current_user.id)
+	# 		current_user.current_cart = @cart
+	# 		current_user.save
+	# 	end
+	# 	current_user.current_cart.add_item(item_id: params[:item_id])
+	# 	current_user.current_cart.save
+	# 	redirect_to cart_path(current_user.current_cart)
+	# end
 
+
+	def create
+		if !current_cart
+			current_user.current_cart = Cart.new
+			current_cart.save
+		end
+		current_cart.add_item(params[:item_id])
+		current_cart.save
+		redirect_to cart_path(current_cart)
+	end
 end
