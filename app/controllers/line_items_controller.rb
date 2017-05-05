3cd182ca842458f2844310e93dc791f037a36363
Kelsey Huse <kelseyhuse30@gmail.com>
2017-05-04 22:11:30 -0400

    begin logic for adding item to cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,3 +1,18 @@
 class LineItemsController < ApplicationController
 
+	def create
+		if !current_cart
+			@cart = current_user.current_cart.build
+			@cart.status = "new"
+		end
+		current_cart.add_item(line_items_params)
+		redirect_to cart_path(current_cart)
+	end
+
+	private
+
+	def line_item_params
+		params.require(:line_item).permit(:item_id)
+	end
+
 end
