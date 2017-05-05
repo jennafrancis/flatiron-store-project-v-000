f8b942eb2441f46788991969d93feb3aea1d8980
Kelsey Huse <kelseyhuse30@gmail.com>
2017-05-05 10:28:26 -0400

    modify logic for Line Items Controller

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,18 +1,14 @@
 class LineItemsController < ApplicationController
 
 	def create
-		if !current_cart
-			@cart = current_user.current_cart.build
-			@cart.status = "new"
+		if !current_user.current_cart
+			@cart = Cart.create(status: "new", user_id: current_user.id)
+			current_user.current_cart = @cart
+			current_user.save
 		end
-		current_cart.add_item(line_items_params)
-		redirect_to cart_path(current_cart)
-	end
-
-	private
-
-	def line_item_params
-		params.require(:line_item).permit(:item_id)
+		current_user.current_cart.add_item(item_id: params[:item_id])
+		current_user.current_cart.save
+		redirect_to cart_path(current_user.current_cart)
 	end
 
 end
